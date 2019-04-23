cefe389d53a991ad47679d29686d6c4bd01139f3
mcraney2 <mcraney2@wisc.edu>
2019-04-23 13:57:04 -0500

    ready to go

--- application/Main.java
+++ application/Main.java
@@ -11,14 +11,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1200,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("1:48 pm Simon");
 	}
 	
 	public static void main(String[] args) {
---
+++ application/MainScreenGui.java
@@ -0,0 +1,10 @@
+package Final_project.application;
+
+public class MainScreenGui {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
